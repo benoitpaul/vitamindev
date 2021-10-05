601f03c0f9053848831fddfbb0ea2d273eb2117d
benoitpaul <benoit.paul@gmail.com>
2021-10-05 07:22:23 -0400

    close menu on desktop (#10)
    Co-authored-by: Benoit Paul <benoitpaul@Benoits-MacBook-Pro.local>

--- cypress/e2e/accessibility.test.ts
+++ cypress/e2e/accessibility.test.ts
@@ -5,16 +5,16 @@ describe('Accessibility tests', () => {
   it('Has no detectable accessibility violations on load', () => {
     cy.checkA11y();
   });
-  it('Navigates to page 2 and checks for accessibility violations', () => {
-    cy.findByText(/go to page 2/i)
-      .click()
-      .checkA11y();
-  });
+  // it('Navigates to page 2 and checks for accessibility violations', () => {
+  //   cy.findByText(/go to page 2/i)
+  //     .click()
+  //     .checkA11y();
+  // });
   it.skip('Focuses on the footer link and asserts its attributes', () => {
-    cy.findAllByText('Gatsby').focus();
+    cy.findAllByText('Benoit Paul').focus();
     cy.focused()
-      .should('have.text', 'Gatsby')
-      .should('have.attr', 'href', 'https://www.gatsbyjs.com')
+      .should('have.text', 'Benoit Paul')
+      .should('have.attr', 'href', 'https://www.benoitpaul.com')
       .should('not.have.css', 'outline-width', '0px');
   });
 
