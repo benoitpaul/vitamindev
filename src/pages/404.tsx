77dc08b2bf551bb8aec846992e476b128c50aaf1
Benoit Paul <benoitpaul@Benoits-MacBook-Pro.local>
2021-10-16 17:54:38 -0400

    custom 404 page

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,13 +1,27 @@
+import { Link } from 'gatsby';
 import React, { FC } from 'react';
+import styled from 'styled-components';
 
 import Layout from '../components/Layout';
 import Seo from '../components/Seo';
+import StyledContainer from '../styles/StyledContainer';
+
+const StyledSection = styled.section`
+  padding: 2em 1em;
+`;
 
 const NotFoundPage: FC<void> = () => (
   <Layout>
     <Seo title="404: Not found" />
-    <h1>404: Not Found</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+    <StyledContainer>
+      <StyledSection>
+        <h1>404: Not Found</h1>
+        <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+        <p>
+          Use the menu or go back <Link to="/">home</Link>
+        </p>
+      </StyledSection>
+    </StyledContainer>
   </Layout>
 );
 
